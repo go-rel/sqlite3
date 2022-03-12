db9cac2a0f8c87e8e4e010bee1a1c56727baf2f1
Surya Asriadie <surya.asriadie@gmail.com>
2022-03-12 18:19:21 +0900

    Basic support for insert with on conflict (#19)

--- sqlite3_test.go
+++ sqlite3_test.go
@@ -62,6 +62,10 @@ func TestAdapter_specs(t *testing.T) {
 	specs.InsertBelongsTo(t, repo)
 	specs.Inserts(t, repo)
 	specs.InsertAll(t, repo)
+	specs.InsertOnConflictIgnore(t, repo)
+	specs.InsertOnConflictReplace(t, repo)
+	specs.InsertAllOnConflictIgnore(t, repo)
+	specs.InsertAllOnConflictReplace(t, repo)
 	// specs.InsertAllPartialCustomPrimary(t, repo) - not supported
 
 	// Update Specs
